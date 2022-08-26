a6a95b4b608f664000d465ccca0e897de2780603
Adam <adam.salvius@gmail.com>
2022-08-26 15:30:09 +0200

    removed export

--- models/rooms.model.js
+++ models/rooms.model.js
@@ -1,12 +1,12 @@
 const db = require("../config/db");
-export async function getRooms() { 
+ async function getRooms() { 
     const sql = `SELECT * FROM rooms`
 
     const result = await db.query(sql)
     return result.rows
 }
 
-export async function getRoomsByName() { 
+ async function getRoomsByName() { 
     const sql = `SELECT name FROM rooms`
 
     const result = await db.query(sql)
